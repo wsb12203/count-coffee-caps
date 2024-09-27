e078586e35e89d2b5da370e9fe68c3f09a3eaa64
wsb12203 <ishoro13@gmail.com>
2024-09-27 13:03:28 +0100

    Update count_coffee_caps.py

--- count_coffee_caps.py
+++ count_coffee_caps.py
@@ -27,19 +27,29 @@ plt.imshow(im)
 plt.show()
 # Find seeds
 cnts, _ = cv2.findContours(im, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-
+# sorted_ctrs = sorted(cnts, key=lambda ctr: cv2.boundingRect(ctr)[1])
 # Convert GRAY instances to BGR
 im = cv2.cvtColor(im, cv2.COLOR_GRAY2BGR)
 
 # Draw circle around detected seeds
 c = 0
 for cnt in cnts:
+    CENTER = (x+w//2, y+h//2)
     x, y, w, h = cv2.boundingRect(cnt)
     # cv2.circle(img, (x+w//2, y+h//2), max(w, h)//2, (0, 0, 255), 3)
     
     cv2.ellipse(img, (x+w//2, y+h//2), (max(w, h)//2,min(w, h)//2), 0, 0, 360, (0, 0, 255), 3)
     # cv2.drawContours(img, [cnt], 0, (0,255,0), 3)
-    c += 5
+    c += 1
+    
+    
+    TEXT_FACE = cv2.FONT_HERSHEY_DUPLEX
+    TEXT_SCALE = 1
+    TEXT_THICKNESS = 2
+    TEXT = str(c)
+    text_size, _ = cv2.getTextSize(TEXT, TEXT_FACE, TEXT_SCALE, TEXT_THICKNESS)
+    text_origin = (CENTER[0] - text_size[0] // 2 - 4, CENTER[1] + text_size[1] // 2)
+    cv2.putText(img, TEXT, CENTER, TEXT_FACE, TEXT_SCALE, (127,255,127), TEXT_THICKNESS, cv2.LINE_AA)
 
 cv2.imwrite(output_file, img)
 
